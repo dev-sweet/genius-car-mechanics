6ce7241f221f0976ef7eb986e772325d9e994f4d
dev-sweet <sweetali0520@gmail.com>
2021-10-29 12:55:24 +0600

    make a small change

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -5,7 +5,7 @@ import { Container } from 'react-bootstrap';
 const Services = () => {
   const [services, setServices] = useState([]);
   useEffect(() => {
-    fetch('http://localhost:5000/services')
+    fetch('https://stormy-springs-38622.herokuapp.com/services')
       .then((res) => res.json())
       .then((data) => setServices(data));
   }, []);
